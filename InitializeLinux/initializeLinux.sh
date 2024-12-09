7742fe73f2e7c901c2571145bd81d12743d596bd
root-5 <hujisannrokuoumunaku.shigoto@gmail.com>
2024-12-09 23:59:31 +0900

    InitializeLinux 追加

---
+++ InitializeLinux/initializeLinux.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# フェイルセーフなどはしていないので、個別に分けて実行すること
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+# ==============================================================================
+# Linux の初期設定を行うスクリプト（Debian）
+# ==============================================================================
+
+# -e（エラー発生でシェル終了）、-u（未設定の変数をエラーにする）、-o pipefail（パイプラインで一つでも失敗があればエラーにする）
+set -euo pipefail
+
+# apt のアップデート
+echo "apt のアップデート"
+apt-get update
+apt-get upgrade -y
+echo ">> OK"
+
+# unzip のインストール
+echo "unzip のインストール"
+apt-get install -y unzip
+echo ">> OK"
+
+# jq のインストール
+echo "jq のインストール"
+apt-get install -y jq
+echo ">> OK"
+
+# curl のインストール
+echo "curl のインストール"
+apt-get install -y curl
+echo ">> OK"
+
+# VSCode 連携に必要な wget のインストール
+apt-get install -y wget
+
+# ==============================================================================
+# Git のインストール
+# ==============================================================================
+
+# Git のインストール
+echo "Git のインストール"
+apt-get install -y git
+
+# Git の SSH 
+echo "Git の SSH 設定"
+cd ~/
+mkdir .ssh
+cd .ssh
+ssh-keygen -t rsa
+touch config
+cat << EOS >> config
+Host github.com
+  HostName github.com
+  IdentityFile ~/.ssh/id_rsa
+  User git
+  Port 22
+  TCPKeepAlive yes
+  IdentitiesOnly yes
+EOS
+echo ">> OK"
+
+# ユーザー名とメールアドレスの設定
+echo "ユーザー名とメールアドレスの設定"
+git config --global user.name "root-5"
+git config --global user.email "hujisannrokuoumunaku.shigoto@gmail.com"
+echo ">> OK"
+
+# ==============================================================================
+# Docker のインストール
+# ==============================================================================
+
+# # Docker のインストール
+# echo "Docker のインストール"
+# apt-get install -y docker.io
+# systemctl start docker
+# systemctl enable docker
+
+# # docker-compose のインストール
+# echo "docker-compose のインストール"
+# apt-get install -y docker-compose
+
+# 安全な変数展開、空白文字などで意図せぬ挙動にならない
+# echo "${arg1}"
+
+# ==============================================================================
+# 言語のインストール
+# ==============================================================================
+
+# Go のインストール
+echo "Go のインストール"
+sudo apt-get install -y golang-go
+
+# ==============================================================================
+# bash の設定
+# ==============================================================================
+
+# .bashrc に追加
+echo "bash の設定"
+cat << EOS >> ~/.bashrc
+# bash のデザインを変更
+PS1='\n\n\[\e[1m\]\[\e[36m\]\u@\h) \[\e[0m\] \[\e[1m\]\w\[\e[0m\]\n└────── \[\e[32m\]\[\e[1m\]$ \[\e[0m\]'
+EOS
--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 ちょっとした制作物、試算品用のリポジトリです。
 各ディレクトリには、そのプロジェクトの概要を記載したREADME.mdがあります。
 
+## FormAttacker
+Web フォームの入力内容を自動化して送信するためのツールです。
+
+## HtmlMachineEdit
+Node.js の jsdom を使って HTML ファイルを DOM として読み込み、JavaScript として編集・保存するためのツールです。
+
+## InitializeLinux
+Linux 環境の初期設定を行うためのシェルスクリプトです。
+
+## JavascriptAndCss
+Chrome 拡張機能「User JavaScript and CSS」で使うスクリプトテンプレートです。
